462d68b4c0512a24024dece3b60391f54ea4c87c
e-landeros <landerosedgard@gmail.com>
2018-12-10 19:44:10 -0800

    setup db connection

--- companyblog/__init__.py
+++ companyblog/__init__.py
@@ -1,8 +1,20 @@
-#companyblog/__init__.py
+# companyblog/__init__.py
 #connect blueprints and login manager 
 # to organize app
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 app = Flask(__name__)
+
+###### DATABASE SETUP ########
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']  = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
 
 from companyblog.core.views import core
 from companyblog.error_pages.handlers import error_pages
--- companyblog/models.py
+++ companyblog/models.py
@@ -1,1 +1,12 @@
-#user modela and blog post model
+from companyblog import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    profile_image = db.Column(db.String(64), null=False, default = 'default_profile.png')# will be a link to a user file 
+    email = db.Column(db.String(64), unique=True, index=True) # index true allows you to make the column an index to connect to other tables
+    username = db.Column(db.String(64), unique=True, index=True)
+
+class BlogPost(db.Model):
+    pass
