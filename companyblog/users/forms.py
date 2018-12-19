9ea5665ff17e19640b9cba9eebdf1353676e770b
e-landeros <landerosedgard@gmail.com>
2018-12-18 17:10:32 -0800

    created user form for updatign profile

--- companyblog/users/forms.py
+++ companyblog/users/forms.py
@@ -17,6 +17,7 @@ class RegistrationForm(FLaskForm):
     username = StringField('UserName', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired(), EqualTo('pass_confirm', message='Passwords must match!')])
     pass_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
 
     def check_email(self,field):
         # user model from import 
@@ -28,3 +29,18 @@ class RegistrationForm(FLaskForm):
         if User.query.filter_by(username=field.data).first():
             raise ValidationError(' Your Username Has Already Been Registered')
 
+class UpdateUserForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('UserName', validators=[DataRequired()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
+
+    def check_email(self,field):
+        # user model from import 
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError(' Your Email Has Already Been Registered')
+
+    def check_username(self,field):
+        # user model from import 
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError(' Your Username Has Already Been Registered')
