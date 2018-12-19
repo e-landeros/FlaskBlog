f2e503253a9ef29802a510e456691a1fd876e937
e-landeros <landerosedgard@gmail.com>
2018-12-18 17:24:25 -0800

    added function to deal with picture upload by user

---
+++ companyblog/users/picture_handler.py
@@ -0,0 +1,17 @@
+import os 
+from PIL import Image
+from flask import url_for, current_app
+
+def add_profile_pic(pic_upload, username):   #gets filename from the os on upload
+    filename = pic_upload.filename
+    ext_type = filename.split('.')[-1]
+    storage_filename = str(username)+'.'+ext_type   # saved as <username>.ext_type, probably better to use hash instead fo uname
+
+    filepath = os.path.join(current_app.root_path, 'static\profile_pics',storage_filename)
+
+    output_size = (200,200)
+    pic = Image.open(pic_upload)
+    pic.thumbnail(output_size)
+    pic.save(filepath)
+
+    return storage_filename
