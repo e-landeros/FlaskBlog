efeaeb0b915391e1c1faca6141e9da5bc2a0d03f
e-landeros <landerosedgard@gmail.com>
2018-12-18 18:14:30 -0800

    completed account page and views function

--- companyblog/users/views.py
+++ companyblog/users/views.py
@@ -43,6 +43,32 @@ def logout():
     logout_user()
     return redirect(url_for('core.index'))
 
-#account(update user)
+#account(update UserForm)
+@users.route('/account', methods=['GET','POST'])
+@login_required
+def account():
+    form = UpdateUserForm()
+    if form.validate_on_sumbit():
+        #if they uploaded pic FileField
+        if form.picture.data:
+            username = current_user.username
+            pic = add_profile_pic(form.picture.data,username)
+            current_user.profile_image = pic
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('User Account Updated')
+        return redirect(url_for('users.account'))
+
+    #not submitting anything we get their current info from form
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    profile_image = url_for('static', filename='profile_pics/'+current_user.profile_image)
+    return render_template('account.html', profile_image=profile_image, form=form)
 
 #show all list of users blog post
+@users.route('/<username>')
+def user_posts(username):
+    page = request.args.get('page',1,type=int)
